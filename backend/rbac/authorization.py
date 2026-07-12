0f751446288066711ef0930b2e163acd8067f951
Himasri <veluruhimasri18@gmail.com>
2026-07-12 23:45:23 +0530

    Removed backend folder

--- backend/rbac/authorization.py
+++
@@ -1,9 +0,0 @@
-from rbac.permissions import permissions
-
-def has_permission(role, permission):
-    allowed_permissions = permissions.get(role, [])
-
-    if "*" in allowed_permissions:
-        return True
-
-    return permission in allowed_permissions
--- backend/rbac/roles.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-class Role(str,Enum):
-    CREATOR="Creator"
-    AGENCY="Agency"
-    MARKETING_TEAM="Marketing Team"
-    ADMINISTRATOR="Administrator"
